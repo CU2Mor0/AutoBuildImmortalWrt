0f95b803a7167449da5e929b94fc487fe146f84c
Wang Xingkai <42564384+CU2Mor0@users.noreply.github.com>
2026-03-20 00:09:06 +0800

    Add 'owut' package to build script

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -60,7 +60,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-
+PACKAGES="$PACKAGES owut"
 
 # 第三方软件包 合并
 # ======== shell/custom-packages.sh =======
